15ace154ace0486ce74a28ba30996ee1845499da
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2014-12-01 10:45:59 +0700

    Initial commit

---
+++ lib/Reply/Plugin/DataDmp.pm
@@ -0,0 +1,36 @@
+package Reply::Plugin::DataDmp;
+
+# DATE
+# VERSION
+
+use strict;
+use warnings;
+
+use base 'Reply::Plugin';
+
+use Data::Dmp 'dmp';
+use overload ();
+
+sub new {
+    my $class = shift;
+    my %opts = @_;
+
+    my $self = $class->SUPER::new(@_);
+    return $self;
+}
+
+sub mangle_result {
+    my $self = shift;
+    my (@result) = @_;
+    return @result ? dump(@result) : ();
+}
+
+1;
+# ABSTRACT: Format results using Data::Dmp
+
+=head1 SYNOPSIS
+
+ ; .replyrc
+ [DataDmp]
+
+=head1 DESCRIPTION
